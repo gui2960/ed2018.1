1242369c023367155ba0b717ff7a0371753703f7
gui2960 <guilhermeb.machado@hotmail.com>
2018-04-18 23:52:27 -0300

    Labirintos

--- 05_Labirinto /construindo_pilha/main.cpp
+++ 05_Labirinto /construindo_pilha/main.cpp
@@ -1,100 +1,138 @@
-#include "libs/ed_base.h"
-#include "libs/ed_mat.h"
-#include <cstdlib>
-#include <ctime>
-#include <fstream>
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <stack>
+# include <libs/ed_base.h>
+# include <libs/ed_mat.h>
+# include  <fstream>
+# include  <iostream>
+# include  <cstdlib>
+# include  <stack>
+# include  <algorithm>  // random_shuffle
+# include  <cstdlib>
+# include  <ctime>
 
-using namespace std;
+using  namespace  std ;
 
-int nlinhas = 20;
-int ncolunas = 35;
+const  char PAREDE = 'k' ;
+const  char ABERTO = 'w' ;
+const  char EMBUSCA = 'y' ;
+const  char CAMINHO = 'b' ;
+const  char ABATIDO = 'm' ;
 
-//MAPA DE CORES
-//r red
-//g green
-//b blue
-//m magenta
-//c cyan -
-//y yellow
-//w white
-//k black
+void  showMat (matchar & mat, vector <Par> pilha, Par inicio, Par fim) {
+    mat_draw (mat);
+    for (Par par: pilha)
+        mat_focus (par, 'c' );
+    if( pilha.size() > 0 )
+        mat_focus (pilha. back (), 'k' );
+    mat_focus (inicio, 'g' );
+    mat_focus (fim, 'r' );
+}
 
-const char PAREDE = 'k';
-const char FURADO = 'w';
-const char RETORNO = 'c';
-const char MARCADOR = 'r';
-const char ATENCAO = 'y';
 
-vector<Par> pegar_vizinhos(Par par){
+vector<Par> getNeib(Par par) {
     vector<Par> vizinhos;
-    vizinhos.push_back(Par(par.l + 1, par.c));
-    vizinhos.push_back(Par(par.l - 1, par.c));
-    vizinhos.push_back(Par(par.l, par.c + 1));
-    vizinhos.push_back(Par(par.l, par.c - 1));
+    vizinhos.push_back( Par (par.l + 1 , par. c));
+    vizinhos.push_back( Par (par.l - 1 , par.c ));
+    vizinhos.push_back( Par (par.l , par.c + 1 ));
+    vizinhos.push_back( Par (par.l , par.c - 1 ));
+//     vizinha.push_back (Par (par.l - 1, par.c - 1));
+//     vizinha.push_back (Par (par.l - 1, par.c + 1));
+//     vizinha.push_back (Par (par.l + 1, par.c + 1));
+//     vizinhos.push_back (Par (par.l + 1, par.c - 1));
     return vizinhos;
 }
 
-int contar_vizinhos_furados(matchar &mat, Par par){
-    int cont = 0;
-    for(auto vizinho : pegar_vizinhos(par))
-        if(!mat.is_inside(vizinho) || mat.get(vizinho) != PAREDE)
-            cont++;
+int  countOpen (matchar & mat,Par par) {
+    int cont = 0 ;
+    for(auto vizinho: getNeib (par))
+        if (!mat.is_inside(vizinho) || mat.get(vizinho) != PAREDE)
+            cont ++;
     return cont;
 }
 
-#include <algorithm>
-void furar(matchar &mat, Par par){
-    //PONTOS DE PARADA
-    if(!mat.is_inside(par))//se estiver fora da matriz
-        return;
-    mat_draw(mat);//desenha a matriz
-    mat_focus(par, ATENCAO);//faz uma bolinha em par
-    ed_show();//mostra na tela
+vector<Par> aleatorio(vector <Par> vet) {
+    std :: random_shuffle(vet.begin(), vet.end());
+    return vet;
+}
 
-    if(mat.get(par) != PAREDE)//se nao for arvore
-        return;
+//void  furar (matchar & mat, Par par) {
+//    if(!mat.equals(par, PAREDE) || countOpen(mat, par) > 1 )
+//        return ;
+//    mat. get (par) = ABERTO;
+//    for(Par vizinho: aleatorio(getNeib(par)))
+//        furar (mat, vizinho);
+//}
 
-    if(contar_vizinhos_furados(mat, par) > 1)
-        return;
+void  furar (matchar &mat, Par par) {
+   vector<Par> pilha;
+   pilha.push_back(par);
+   mat.get(par) = ABERTO;
 
-    //ACAO
-    mat.get(par) = FURADO;
+   while(pilha.size()!= 0 ) {
+       vector<Par> tfurar;
+       Par topo = pilha.back();
+       for(auto vizinhos: getNeib(topo))
+           if(countOpen(mat, vizinhos) <= 1 && mat.equals(vizinhos,PAREDE) )
+              tfurar.push_back(vizinhos);
 
-    //DESENHO
-    mat_draw(mat);//desenha a matriz
-    mat_focus(par, MARCADOR);//faz uma bolinha em par
-    ed_show();//mostra na tela
+       if(tfurar.size() != 0 ) {
+           int vizinho = rand ()% tfurar.size();
+           mat.get(tfurar[vizinho]) = ABERTO;
+           pilha.push_back (tfurar[vizinho]);
+           mat_draw (mat);
+           ed_show ();
+       } else{
+           pilha.pop_back();
+       }
+   }
+}
 
-    //RECURSAO
-    vector<Par> vizinhos = pegar_vizinhos(par);
-    std::random_shuffle(begin(vizinhos), end(vizinhos));
+bool findPath (matchar & mat, Par inicio, Par final){
+    vector<Par> pilha;
+    pilha. push_back (inicio);
+    mat. get (inicio) = EMBUSCA;
 
-    for(Par vizinho : vizinhos)
-        furar(mat, vizinho);
+    while (pilha.size() != 0){
+        Par topo = pilha.back();
+        if (topo == final){
+            return  true ;
+        }
+        vector <Par> viz_abertos;
+        for (Par par: getNeib(topo))
+            if (mat.get(par) == ABERTO)
+                viz_abertos. push_back (par);
 
-    //ACAO APOS A RECURSAO
-    mat.get(par) = RETORNO;
 
-    //DESENHO
-    mat_draw(mat);//desenha a matriz
-    mat_focus(par, MARCADOR);//faz uma bolinha em par
-    ed_show();//mostra na tela
+        ed_show();
+        if (viz_abertos.size() == 0 ){
+            pilha. pop_back ();
+            showMat(mat,pilha,inicio,final);
+            ed_show();
+        }
+        else{
+            Par escolhido = viz_abertos[rand()%viz_abertos.size()];
+            mat.get(escolhido) = EMBUSCA;
+            pilha. push_back (escolhido);
+            showMat(mat,pilha,inicio,final);
+            ed_show();
+        }
+    }
+    return  false ;
 }
 
 
-int main(){
-    srand(time(NULL));
-    //cria uma matriz de caracteres de 15 linhas por 20 colunas, e preenche todos os elementos
-    //com 'y'
-    matchar mat(15, 20, PAREDE);
+int  main () {
+    srand (time(NULL));
+    matchar mat ( 10 , 15 , PAREDE);
+    furar (mat, Par(1,1)); // chama uma funзгo recursiva
+    mat_draw (mat);
+    ed_show ();
+    Par inicio = mat_get_click (mat, " digite o local de inicio " );
+    Par final = mat_get_click (mat, " digite o local de fim " );
+    mat_draw (mat);
+    ed_show ();
 
-    furar(mat, Par(1, 1));//chama a função recursiva
-
-    ed_lock();//impede que termine abruptamente
-
-    return 0;
+    findPath (mat, inicio, final);
+    ed_lock (); // impedir que termine abruptamente
+    return  0 ;
 }
+
+
